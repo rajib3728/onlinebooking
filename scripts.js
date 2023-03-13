a5621b5b41ab0e3fc0640cff09d3fadfe0e9e646
Rajib Paul <53445533+rajib3728@users.noreply.github.com>
2023-03-13 12:20:51 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function fun1()
+{
+    window.open("book_ticket.html")
+}
+function fun2()
+{
+    window.open("cancel_ticket.html")
+}
+function fun3()
+{
+    window.open("booking_status.html")
+}
